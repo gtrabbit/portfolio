b1539c550b85a1d8cce11d8386d258c7d0835d68
Chris Rune <gtrabbit@gmail.com>
2017-07-20 13:15:15 -0400

    prepared for remote hosting

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,81 @@
 window.onload = function(){
-	console.log(window.innerHeight)
+	makeEventListeners();
 	makeScrolly();
+
 }
 
+
+const makeEventListeners = function(){
+	//scroll one page
+	document.getElementById('scrollButton').addEventListener('click', function(){
+		for (let i = 0; i<=20; i++){
+			window.scrollTo(0, 0);
+			window.setTimeout(function(){
+				window.scrollBy(0, window.innerHeight/20);
+			}, i*i);
+		}
+	})
+
+	//adds listeners to navigation
+	Array.from(document.getElementsByClassName('navLink')).forEach(function(a){
+		a.addEventListener('click', function(){
+			smoothScroll(a.id.slice(0, -4));
+		})
+	})
+
+
+	//listener for mailing button
+	document.getElementById('contactForm').addEventListener('submit', function(e){
+		e.preventDefault();
+		sendMail();
+	})
+
+
+
+}
+
+
+const showResponse = function(res){
+	document.getElementById('response').textContent = res;
+
+}
+
+const sendMail = function(){
+	let name = document.getElementById('name').value;
+	let email = document.getElementById('email').value;
+	let msg = document.getElementById('message').value;
+	let data = {
+		'name': name,
+		'email': email,
+		'msg': msg
+	}
+	let request = new XMLHttpRequest();
+
+	request.onreadystatechange = function(){
+		if (this.readyState === XMLHttpRequest.DONE && this.status === 200){
+			showResponse(this.responseText);
+		} 
+	}
+
+	request.open('POST', '../portfolio/sendemail', true);
+	request.setRequestHeader('Content-type', "application/json");
+	request.send(JSON.stringify(data));
+
+}
+
+
+const smoothScroll = function(elID){
+	let node = document.getElementById(elID);
+	let speed = Math.ceil(node.offsetTop / 30);
+	for (let i = 0; i <30; i++){
+
+		window.setTimeout(function(){
+				window.scrollBy(0, speed);
+			}, i*i);
+	}
+	node.focus();
+
+}
 
 
 const makeScrolly = function(){
@@ -11,7 +84,7 @@ const makeScrolly = function(){
 	let paraEls = Array.from(document.getElementsByClassName('parallax'));
 	window.onscroll = ()=>{
 		faders.forEach(function(a){
-	 		a.style.opacity =  1 - Math.pow( Math.abs( ( ( (window.innerHeight - (a.scrollHeight *1.5 ) )  /2) - (a.offsetTop - window.scrollY ) ) / (window.innerHeight/2)  ), 3 )
+	 		a.style.opacity =  1 - Math.pow( Math.abs( ( ( (window.innerHeight - (a.scrollHeight  ) )  /2) - (a.offsetTop - window.scrollY ) ) / (window.innerHeight/2)  ), 3 )
 	 		})
 		paraEls.forEach(el=>{
 
@@ -19,7 +92,7 @@ const makeScrolly = function(){
 				&& (document.body.scrollTop  < (el.offsetTop + el.scrollHeight)) ){
 					let value = (el.offsetTop - document.body.scrollTop) / window.innerHeight / 4  //makes a number from 0.25 to -1
 					let paraOffset = value * window.innerHeight;
-					let coords = "50% " + paraOffset / 1.5  + 'px';
+					let coords = "50% " + (paraOffset * 1.2)  + 'px';
 					el.style.backgroundPosition = coords;
 					let colorValue;
 					if (el.dataset.hasOwnProperty('first') && el.dataset.first === "true"){
@@ -27,15 +100,13 @@ const makeScrolly = function(){
 					} else {
 						colorValue = Math.round((255 * .75) + (255 * value));
 					}
-
 					if (el.dataset.hasOwnProperty('inverse') && el.dataset.inverse === "true"){
 						colorValue = 255 - colorValue;
 					}
-
-
 					let color = "rgb(" + colorValue + ", " + colorValue + ", " + colorValue + ")";
 					el.style.backgroundColor = color;
 			}
 		})
 	}
+	
 }
